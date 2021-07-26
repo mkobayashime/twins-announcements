57153c60f4d556c2fc7dc5c1de175249715d0a22
Masaki Kobayashi <contact@mkobayashi.me>
2021-07-27 01:47:59 +0900

    TypeScript/ESLint/Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/no-namespace": 0,
+  },
+};
