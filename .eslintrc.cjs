c84b71a1405a78485b6e122540e995f237569287
Masaki Kobayashi <contact@mkobayashi.me>
2022-10-13 12:50:28 +0900

    Use my ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,30 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["@mkobayashime", "@mkobayashime/typescript"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["import", "@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/no-namespace": 0,
-    "import/order": [
-      2,
-      {
-        alphabetize: { caseInsensitive: true, order: "asc" },
-        groups: [["builtin", "external"], "parent", ["sibling", "index"]],
-        "newlines-between": "always",
-      },
-    ],
-    "import/first": 2,
-  },
+  rules: {},
 };
